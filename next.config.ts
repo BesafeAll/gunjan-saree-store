46364f9d76f18749927aefec930aa6039c185a89
BesafeAll <sarvottamsharma.code@gmail.com>
2026-02-09 09:10:51 +0530

    Configure Next.js for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
+const repo = "gunjan-saree-store";
+
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
+
+  images: {
+    unoptimized: true,
+  },
+
 };
 
 export default nextConfig;
